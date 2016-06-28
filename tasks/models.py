ab064a33e6a044f71c4c7ef568dac252b5ea41fd
Sol <rygula.dawid@gmail.com>
2016-06-28 21:01:29 +0200

    Created first iteration of task models, added urls.py to tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,21 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Families(models.Model):
+    user_id = models.ForeignKey('auth.User', related_name='Family')
+    family_name = models.CharField(max_length=40)
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=300)
+    owner = models.ForeignKey('auth.User', related_name='Task')
+    created_at = models.DateTimeField(auto_now_add=True)
+    reactivated_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    repeatable = models.BooleanField(default=False)
+
+
+class TaskList(models.Model):
+    user_id = models.ForeignKey('auth.User', related_name="Tasks")
+    task_id = models.ForeignKey(Task, related_name="Owner")
+
---
+++ tasks/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from tasks import views
+
+app_name = 'gymstats'  # url namespace
+
+urlpatterns = [
+]
