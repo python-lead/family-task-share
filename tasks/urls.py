0fa6f88448592c0b0b1d0f65d5bdf3785857c239
Sol <rygula.dawid@gmail.com>
2016-09-06 19:41:03 +0200

    Refactoring, and cleaning

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,6 @@ from tasks import views
 # app_name = 'tasks'
 urlpatterns = [
     url(r'^mytasks/$', views.MyListApi.as_view(), name='tasks'),
-    url(r'^tasks/$', views.TaskListView.as_view()),
     url(r'^mytasks/(?P<pk>[0-9]+)/$', views.TaskDetailView.as_view(), name='task'),
 ]
 
